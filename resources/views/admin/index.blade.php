42816955898154d5d1501b67f7abba522d75853c
Rogelio Fuentes Alvarez <rogelioroger281@gmail.com>
2018-05-12 15:29:23 -0500

    Prueba de status

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -7,7 +7,7 @@
 
 <div class="row">
   <div class="valign-wrapper minh-400 col l12">
-    <h1 class="valign center" style="width: 100%">Bienvenido</h1>
+    <h1 class="valign center" style="width: 100%">Bienvenidos</h1>
   </div>
 </div>
 
